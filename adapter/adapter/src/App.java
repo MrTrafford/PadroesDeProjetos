71094d90585068dbb3fa1b3c032e2b29d481cbc5
Ninja Quantum <tiagomdantas2011@gmail.com>
2026-09-12 23:03:05 -0300

    Add client code comment in App main method
    Added comment indicating client code in main method.

--- adapter/adapter/src/App.java
+++ adapter/adapter/src/App.java
@@ -1,5 +1,6 @@
 public class App {
     public static void main(String[] args) throws Exception {
+        //código cliente
         System.out.println("Hello, World!");
         ProcessaPagamento pagamento = new PixAdapter(new PagamentoPix());
         pagamento.pagar(10.50);
